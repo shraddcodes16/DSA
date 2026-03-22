3f49b0c02332313020e251a441d72c62afcbd866
shraddcodes16 <shraddhathalpati3@gmail.com>
2026-03-22 10:37:13 +0530

    Evenodd

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int i=6;
+
+        if(i%2==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
